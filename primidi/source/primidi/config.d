73218ef8ff31aaa979cbe26465d26bbafe6d8c3b
Enalye <enalyen@gmail.com>
2019-11-27 11:49:46 +0100

    Small fixes in config

--- primidi/source/primidi/config.d
+++ primidi/source/primidi/config.d
@@ -7,13 +7,15 @@ import primidi.player, primidi.midi, primidi.locale;
 private enum _configFilePath = "config.json";
 
 void loadConfig() {
-    if(!exists(_configFilePath))
+    if(!exists(_configFilePath)) {
+        saveConfig();
         return;
+    }
     JSONValue json = parseJSON(readText(_configFilePath));
-    string inputName = getJsonStr(json, "input");
-    string outputName = getJsonStr(json, "output");
-    string scriptPath = absolutePath(buildNormalizedPath(getJsonStr(json, "script")));
-    string localePath = absolutePath(buildNormalizedPath(getJsonStr(json, "locale")));
+    string inputName = getJsonStr(json, "input", "");
+    string outputName = getJsonStr(json, "output", "");
+    string scriptPath = absolutePath(buildNormalizedPath(getJsonStr(json, "script")), "");
+    string localePath = absolutePath(buildNormalizedPath(getJsonStr(json, "locale")), "");
     
     selectMidiInDevice(mnFetchInput(inputName));
     selectMidiOutDevice(mnFetchOutput(outputName));
@@ -25,10 +27,8 @@ void saveConfig() {
     JSONValue json;
     auto midiIn = getMidiIn();
     auto midiOut = getMidiOut();
-    if(midiIn && midiIn.port)
-        json["input"] = midiIn.port.name;
-    if(midiOut && midiOut.port)
-        json["output"] = midiOut.port.name;
+    json["input"] = (midiIn && midiIn.port) ? midiIn.port.name : "";
+    json["output"] = (midiOut && midiOut.port) ? midiOut.port.name : "";
     json["script"] = relativePath(buildNormalizedPath((getScriptFilePath())));
     json["locale"] = relativePath(buildNormalizedPath(getLocale()));
 
